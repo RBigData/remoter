1d7b761c01c0467c30192b36ccf7269f2a9c3c57
wrathematics <wrathematics@gmail.com>
2015-03-02 16:30:02 -0500

    switched to pbdRscript in unified interface

--- tests/unified.r
+++ tests/unified.r
@@ -1,29 +1,21 @@
-library(rzmq)
-library(parallel)
-source("../R/repl.r")
+library(pbdCS)
+suppressMessages(source("../R/repl.r"))
 
 
 start_pbd_servers <- function(ranks=2, bcast_method="zmq", port=5555)
 {
   ### FIXME replace first 3 lines with library(pbdCS) eventually
   rscript <- paste0("
-    library(rzmq)
-    library(parallel)
+    suppressPackageStartupMessages(library(pbdCS))
     source('../R/repl.r')
     pbdenv$whoami <- 'remote'
     pbdenv$port <- ", port, "
-    pbdenv$bcast_method <- \\\"", bcast_method, "\\\"
+    pbdenv$bcast_method <- \"", bcast_method, "\"
     pbd_repl()
     finalize()
   ")
   
-  rscript <- gsub(rscript, pattern="\\n", replacement="; ")
-  rscript <- gsub(rscript, pattern=" +", replacement="")
-  rscript <- sub(rscript, pattern="^;", replacement="")
-  rscript <- gsub(rscript, pattern="$", replacement="\\$", fixed=TRUE)
-  
-  expr <- paste("mpirun -np", ranks, "Rscript -e \"", rscript, "\"")
-  mcparallel(system(expr, wait=FALSE), detached=TRUE)
+  pbdRscript(rscript, cores=ranks, auto=TRUE, pid=FALSE, wait=FALSE)
   
   invisible(TRUE)
 }
@@ -38,7 +30,6 @@ start_pbd_client <- function(bcast_method="zmq", port=5555)
   
   invisible(TRUE)
 }
-
 
 
 start_pbd_servers()
