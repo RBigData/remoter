1769b64fd33a49cf7b406751ecb6d1c30ef449d4
wrathematics <wrathematics@gmail.com>
2015-05-12 14:01:12 -0400

    added package loading filter

--- R/repl.r
+++ R/repl.r
@@ -256,6 +256,32 @@ pbd_bcast <- function(msg)
 
 
 
+pbd_eval_filter_server <- function(msg)
+{
+  if (all(grepl(x=msg, pattern="^library\\(", perl=TRUE)))
+  {
+    if (comm.rank() == 0)
+    {
+      msg <- paste0("
+        tmp <- file(tempfile())
+        sink(tmp, append=TRUE)
+        sink(tmp, append=TRUE, type='message')\n", 
+        msg, "\n
+        sink()
+        sink(type='message')
+        cat(paste(readLines(tmp), collapse='\n'))
+        unlink(tmp)
+      ")
+    }
+    else
+      msg <- paste0("suppressPackageStartupMessages(", msg, ")")
+  }
+  
+  msg
+}
+
+
+
 pbd_eval <- function(input, whoami, env)
 {
   set.status(continuation, FALSE)
@@ -296,8 +322,9 @@ pbd_eval <- function(input, whoami, env)
       msg <- NULL
     
     msg <- pbd_bcast(msg)
-    
     barrier() # just in case ...
+    
+    msg <- pbd_eval_filter_server(msg=msg)
     
     ret <- 
     withCallingHandlers(
