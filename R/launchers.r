3402f040ca388a4440717dea008fc8d3040de4c5
wrathematics <wrathematics@gmail.com>
2015-10-21 10:38:06 -0400

    re-add docs

--- R/launchers.r
+++ R/launchers.r
@@ -1,3 +1,36 @@
+#' Client/Server Launchers
+#' 
+#' Launchers for the remoter client/server.
+#' 
+#' @param port
+#' The port (number) that will be used for communication between 
+#' the client and server.
+#' @param showmsg
+#' Logical; if TRUE, turns on the "debug mode" for the server,
+#' and prints messages in the server terminal.
+#' @param remote_addr
+#' The remote host/address/endpoint.
+#' 
+#' @details
+#' The \code{port} values between the client and server \emph{MUST}
+#' agree.  If they do not, this can cause the client to hang.
+#' 
+#' The client is a specialized REPL that intercepts commands sent
+#' through the R interpreter.  These commands are then sent from the
+#' client to and evaluated on the server.
+#' The client communicates over ZeroMQ with the server using a REQ/REP pattern.
+#' Both commands (from client to server) and returns (from server
+#' to client) are handled in this way.
+#' 
+#' To shut down the server and the client, use the command \code{q()}.
+#' 
+#' @name launchers
+#' @rdname launchers
+NULL
+
+
+
+#' @rdname launchers
 #' @export
 server <- function(port=55555, showmsg=FALSE)
 {
@@ -9,6 +42,9 @@ server <- function(port=55555, showmsg=FALSE)
   invisible(TRUE)
 }
 
+
+
+#' @rdname launchers
 #' @export
 client <- function(remote_addr, port=55555)
 {
--- R/remoter-package.r
+++ R/remoter-package.r
@@ -2,14 +2,11 @@
 #'
 #' @name remoter-package
 #' 
-#' @import methods
 #' @import pbdZMQ
 #' 
 #' @importFrom stats runif
 #' @importFrom utils capture.output globalVariables
 #'
-#' @useDynLib remoter
-#' 
 #' @docType package
 #' @author Drew Schmidt and Wei-Chen Chen
 #' @keywords package
