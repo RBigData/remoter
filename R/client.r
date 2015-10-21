07a765d13a33f26203204f33fe303bd28ffd701a
wrathematics <wrathematics@gmail.com>
2015-10-21 17:43:47 -0400

    split launchers, update with new features

--- R/client.r
+++ R/client.r
@@ -1,15 +1,15 @@
-#' Client/Server Launchers
+#' Client Launcher
 #' 
 #' Launchers for the remoter client/server.
 #' 
-#' @param port
-#' The port (number) that will be used for communication between 
-#' the client and server.
-#' @param showmsg
-#' Logical; if TRUE, turns on the "debug mode" for the server,
-#' and prints messages in the server terminal.
 #' @param remote_addr
 #' The remote host/address/endpoint.
+#' @param port
+#' The port (number) that will be used for communication between 
+#' the client and server.  The port value for the client and server
+#' must agree.
+#' @param prompt
+#' The prompt to use to delineate the client from the normal R REPL.
 #' 
 #' @details
 #' The \code{port} values between the client and server \emph{MUST}
@@ -24,31 +24,11 @@
 #' 
 #' To shut down the server and the client, use the command \code{q()}.
 #' 
-#' @name launchers
-#' @rdname launchers
-NULL
-
-
-
-#' @rdname launchers
 #' @export
-server <- function(port=55555, showmsg=FALSE)
-{
-  pbdenv$whoami <- 'remote'
-  pbdenv$port <- port
-  pbdenv$debug <- showmsg
-  remoter_repl()
-  
-  invisible(TRUE)
-}
-
-
-
-#' @rdname launchers
-#' @export
-client <- function(remote_addr, port=55555)
+client <- function(remote_addr, port=55555, prompt="remoteR")
 {
   pbdenv$whoami <- "local"
+  pbdenv$prompt <- prompt
   pbdenv$port <- port
   pbdenv$remote_addr <- remote_addr
   remoter_repl()
---
+++ R/server.r
@@ -0,0 +1,30 @@
+#' Server Launcher
+#' 
+#' Launcher for the remoter server.
+#' 
+#' @param port
+#' The port (number) that will be used for communication between 
+#' the client and server.  The port value for the client and server
+#' must agree.
+#' @param password
+#' A password the client must enter before the user can process
+#' commands on the server.
+#' @param maxretry
+#' The maximum number of retries for passwords before shutting
+#' everything down.
+#' @param showmsg
+#' Logical; if TRUE, turns on the "debug mode" for the server,
+#' and prints messages in the server terminal.
+#' 
+#' @export
+server <- function(port=55555, password=NULL, maxretry=5, showmsg=FALSE)
+{
+  pbdenv$whoami <- "remote"
+  pbdenv$port <- port
+  pbdenv$debug <- showmsg
+  pbdenv$password <- password
+  remoter_repl()
+  
+  invisible(TRUE)
+}
+
