608852c560824f759b03b76f69514e65e883258e
wrathematics <wrathematics@gmail.com>
2015-10-26 05:46:43 -0400

    add get and compare versions utils

--- R/utils.r
+++ R/utils.r
@@ -4,3 +4,25 @@ validate_port <- function(port)
   assert_that(port < 65536)
 }
 
+
+
+get_versions <- function()
+{
+  pkgs <- c("pbdZMQ", "remoter")
+  ret <- lapply(pkgs, packageVersion)
+  names(ret) <- pkgs
+  
+  ret
+}
+
+
+
+compare_versions <- function(client, server)
+{
+  if (client$pbdZMQ < server$pbdZMQ)
+    return(FALSE)
+  if (client$remoter < server$remoter)
+    return(FALSE)
+  
+  TRUE
+}
