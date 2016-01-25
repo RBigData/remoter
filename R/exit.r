6d5898d49502021e34b6cbd855a2f6d5b239b5b7
wrathematics <wrathematics@gmail.com>
2016-01-25 09:56:28 -0500

    Update changelog, namespace

---
+++ R/exit.r
@@ -0,0 +1,34 @@
+#' exit
+#' 
+#' Exit the remoter client/server.
+#' 
+#' @description
+#' This function cleanly shuts down the remoter server the client
+#' is currently connected to, as well as shutting down the client.
+#' One can also use \code{q()} (while the client is running), and
+#' this will not close the active R session on the client.
+#' 
+#' 
+#' @param client.only
+#' Logical; if \code{TRUE}, then the client disconnects from
+#' the server.  Otherwise, the server is shut down together 
+#' with the client.
+#' 
+#' @return
+#' Returns \code{TRUE} invisibly on successful exit.
+#' 
+#' @export
+exit <- function(client.only=TRUE)
+{
+  if (!assert_nostop(is.flag(client.only))) return(invisible(FALSE))
+  
+  if (!client.only || .pbdenv$whoami == "local")
+    set.status(should_exit, TRUE)
+  
+  return(invisible(TRUE))
+}
+
+
+
+### For internal consistency
+remoter_exit <- exit
