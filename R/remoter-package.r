4c1d748685bf0ca91c36c89da1c6114f1b650ba3
wrathematics <wrathematics@gmail.com>
2016-02-03 17:46:05 -0500

    add project url everywhere

--- R/remoter-package.r
+++ R/remoter-package.r
@@ -4,15 +4,16 @@
 #' remoter, you can use RStudio running on your laptop to execute
 #' commands on an R session running on, for example, Amazon's EC2.
 #' 
+#' @references Project URL: \url{https://github.com/wrathematics/remoter}
+#' @author Drew Schmidt and Wei-Chen Chen
+#' 
 #' @name remoter-package
 #' 
 #' @import pbdZMQ
-#' 
 #' @importFrom stats runif
 #' @importFrom utils capture.output globalVariables packageVersion
 #' @import assertthat
 #'
 #' @docType package
-#' @author Drew Schmidt and Wei-Chen Chen
 #' @keywords package
 NULL
