1233435aa975cceea62de4ad10df14332061670c
wrathematics <wrathematics@gmail.com>
2016-02-05 22:32:02 -0500

    fix cran checks

--- R/zzz.r
+++ R/zzz.r
@@ -1,16 +1,22 @@
 .onLoad <- function(libname, pkgname)
 {
-  test <- tryCatch(require("sodium", quietly=TRUE), warning=identity)
-  if (inherits(test, c("simpleWarning", "warning")))
-  {
-    packageStartupMessage("\nremoter NOTE: Unable to load 'sodium'. You will not be able to launch or connect to secure servers.")
-    set(withsodium, FALSE)
-  }
-  else
+  test <- requireNamespace("sodium", quietly=TRUE)
+  if (test)
   {
     generate_keypair()
     set(withsodium, TRUE)
   }
+  else
+    set(withsodium, FALSE)
   
   invisible()
 }
+
+
+
+### doesn't resolve in the right order...
+# .onAttach <- function(libname, pkgname)
+# {
+#   if (!has.sodium())
+#     packageStartupMessage("\nremoter NOTE: Unable to load 'sodium'. You will not be able to launch or connect to secure servers.")
+# }
