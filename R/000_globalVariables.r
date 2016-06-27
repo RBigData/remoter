6b8ed64f39cbf0908aa78015bb9082853e4f3fb8
Drew Schmidt <wrathematics@gmail.com>
2016-06-27 16:47:53 -0400

    fix false global warning

--- R/000_globalVariables.r
+++ R/000_globalVariables.r
@@ -7,5 +7,6 @@ utils::globalVariables(c(
   "socket", "password", "maxattempts", "status", "context", "port", 
   "shouldwarn", "num_warnings", "prompt", "client_lasterror",
   "sendport", "recvport", "timer", ".rDevices", 
-  "need_auto_rhelp_on", "need_auto_rpng_off"
+  "need_auto_rhelp_on", "need_auto_rpng_off",
+  "isbatch"
 ))
