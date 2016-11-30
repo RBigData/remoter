f0b2a6eefbd4d124199d1744738d378db87dfe70
Drew Schmidt <wrathematics@gmail.com>
2016-11-30 09:45:56 -0500

    use fancy latex

--- vignettes/build_pdf.sh
+++ vignettes/build_pdf.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
+fixVersion(){
+  PKGVER=`grep "Version:" ../DESCRIPTION | sed -e "s/Version: //"`
+  sed -i -e "s/myversion{.*}/myversion{${PKGVER}}/" $1
+}
+
 cleanVignette(){
   rm -f *.aux *.bbl *.blg *.log *.out *.toc *.dvi
 }
 
 buildVignette(){
+  fixVersion $1
   pdflatex $1
   bibname=`echo "$1" | sed -e 's/\..*//'`
   bibtex $bibname
-  #makeindex $1
-  #pdflatex $1
   pdflatex $1
   pdflatex $1
   Rscript -e "tools::compactPDF('$1', gs_quality='ebook')"
