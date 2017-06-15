94fd96e4f40923689b425be409a62e59ac8b4276
wrathematics <wrathematics@gmail.com>
2017-06-15 09:53:52 -0400

    timestamp functioning in latest RStudio

--- R/wcc_addhistory.r
+++ R/wcc_addhistory.r
@@ -6,19 +6,7 @@ addhistory <- function(read)
   if (!grepl(x = tmp, pattern = paste0(suffix, "$"), perl = TRUE))
     tmp <- paste0(tmp, suffix)
 
-  ### TODO: need to check for OS X, X11, ... etc.
-  ###       check if hangs, then check if up/down recalls work.
-  if (isWindows() && isRStudio())
-  {
-    ### This adds history to native R but is insufficient for Rtudio IDE.
-    # .rs.registerReplaceHook("timestamp", "utils", function(original, ...)
-    # {
-    #   invisible(.Call("rs_timestamp", "bb"))
-    # })
-    # timestamp()
-  }
-  else
-    utils::timestamp(stamp = tmp, prefix = "", suffix = "", quiet = TRUE)
+  utils::timestamp(stamp = tmp, prefix = "", suffix = "", quiet = TRUE)
 
   invisible()
 }
